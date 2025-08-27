fca2ed819f053b0c95fabab2751e86a81a5eb62f
James Baker <22352629+jbake0070@users.noreply.github.com>
2025-08-27 11:38:12 -0600

    Testing move.js for serverless communication

---
+++ api/move.js
@@ -0,0 +1,12 @@
+let games = {}; 
+
+export default function handler(req, res) {
+  const { lobby, move } = req.body;
+
+  if (!games[lobby]) {
+    games[lobby] = { moves: [] };
+  }
+  games[lobby].moves.push(move);
+
+  res.status(200).json({ status: 'ok' });
+}
